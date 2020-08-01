0da8c43903673e45bad1af98ba9ea6519f61a1f5
yuktaatreya <44002986+yuktaatreya@users.noreply.github.com>
2020-08-01 14:26:36 +0530

    Add files via upload

---
+++ lib/start_building.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import './home_page.dart';
+
+class StepperClass extends StatefulWidget {
+  @override
+  _StepperClassState createState() => new _StepperClassState();
+}
+
+class _StepperClassState extends State<StepperClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Stack(
+          children: <Widget>[
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.only(top: 10),
+                    padding: const EdgeInsets.only(left: 40.0, right: 8.0),
+                    child: Text(
+                      'Start Building',
+                      style: TextStyle(fontFamily: 'Open Sans', fontSize: 23),
+                    )),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  //margin: EdgeInsets.only(left: 83, right: 0),
+                  child: FlatButton(
+                    color: Colors.blue[900],
+                    child: Image.asset(
+                      'assets/images/logo1.jpeg',
+                      height: 50,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}

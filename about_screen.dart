ab604ef05bec93518e85a71196f3995d8b179193
yuktaatreya <44002986+yuktaatreya@users.noreply.github.com>
2020-08-01 14:25:32 +0530

    Add files via upload

---
+++ about_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:rwh_assistant/home_page.dart';
+
+class AboutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Row(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                padding: const EdgeInsets.only(left: 80.0, right: 8.0),
+                child: Text(
+                  'About Us',
+                  style: TextStyle(fontFamily: 'Open Sans', fontSize: 23),
+                )),
+            Container(
+              margin: EdgeInsets.only(left: 50, right: 0),
+              child: FlatButton(
+                color: Colors.blue[900],
+                child: Image.asset(
+                  'assets/images/logo1.jpeg',
+                  height: 50,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}

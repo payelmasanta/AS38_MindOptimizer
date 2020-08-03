6b8c8ae24c40c155f86ef16bd4475cd5d57e6a02
Payel <61454146+payelmasanta@users.noreply.github.com>
2020-08-03 07:58:23 +0530

    Update loading.dart

--- lib/loading.dart
+++ lib/loading.dart
@@ -5,7 +5,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.blue[900],
       child: Center(
         child: SpinKitCircle(
           color: Colors.white,
